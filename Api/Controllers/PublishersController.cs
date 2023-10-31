fa85f3292ec2cb660c41a8ffbe3618d737720b7d
Vaczi, Imre <imre.vaczi@zeiss.com>
2023-10-31 08:38:40 +0100

    Implement PublishersController endpoints

--- Api/Controllers/PublishersController.cs
+++ Api/Controllers/PublishersController.cs
@@ -5,15 +5,17 @@ using Api.Requests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace src.Controllers;
+namespace Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
 public class PublishersController : ControllerBase
 {
+    private readonly BookStoreDbContext bookStoreDbContext;
     public PublishersController(BookStoreDbContext bookStoreDbContext)
     {
+        this.bookStoreDbContext = bookStoreDbContext;
     }
 
     [HttpGet(Name = "GetPublishers")]
@@ -21,24 +23,41 @@ public class PublishersController : ControllerBase
     [ProducesResponseType(typeof(List<Publisher>), StatusCodes.Status200OK)]
     public async Task<ActionResult<List<Publisher>>> GetPublishers()
     {
-        return null;
+        var publishers = await bookStoreDbContext.Publishers.ToListAsync();
+
+        return publishers;
     }
     
     [HttpGet]
-    [Route("{id:int}", Name = "GetPublisherById")]
+    [Route("{publisherId:int}", Name = "GetPublisherById")]
     [Consumes(MediaTypeNames.Application.Json)]
     [ProducesResponseType(typeof(Publisher), StatusCodes.Status200OK)]
-    public async Task<ActionResult<string>> GetPublisherById(int publisherId)
+    public async Task<IActionResult> GetPublisherById(int publisherId)
     {
-        return null;
+        var publisher = await bookStoreDbContext.Publishers.FindAsync(publisherId);
+        if (publisher is null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(publisher);
     }
 
     [HttpPost(Name = "CreatePublisher")]
     [Consumes(MediaTypeNames.Application.Json)]
     [ProducesResponseType(typeof(Publisher), StatusCodes.Status201Created)]
-    public ActionResult<string> CreatePublisher(CreatePublisherRequest createPublisherRequest)
+    public async Task<ActionResult<Publisher>> CreatePublisher(CreatePublisherRequest createPublisherRequest)
     {
-        return null;
+        var publisher = new Publisher
+        {
+            Name = createPublisherRequest.Name
+        };
+
+        bookStoreDbContext.Publishers.Add(publisher);
+
+        var numberOfRowsInserted = await bookStoreDbContext.SaveChangesAsync();
+
+        return publisher;
     }
 
     [HttpPut(Name = "UpdatePublisher")]
@@ -46,6 +65,10 @@ public class PublishersController : ControllerBase
     [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
     public async Task<IActionResult> UpdatePublisher(UpdatePublisherRequest updatePublisherRequest)
     {
+        var publisherToUpdate = await bookStoreDbContext.Publishers.FindAsync(updatePublisherRequest.Id);
+        publisherToUpdate.Name = updatePublisherRequest.Name;
+        await bookStoreDbContext.SaveChangesAsync();
+        
         return NoContent();
     }
 
@@ -55,6 +78,10 @@ public class PublishersController : ControllerBase
     [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
     public async Task<IActionResult> DeletePublisher(int id)
     {
+        var publisherToDelete = await bookStoreDbContext.Publishers.FindAsync(id);
+        bookStoreDbContext.Publishers.Remove(publisherToDelete);
+        await bookStoreDbContext.SaveChangesAsync();
+        
         return NoContent();
     }
 }
